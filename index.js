ba158e7aad7e27a8cf1b697300173182872720eb
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-08-06 01:43:31 -0500

    mostly working now:

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMd = require('./generateMd');
+const generateMd = require('./utils/generateMd');
 const fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'please enter project name',
+        name: 'Title',
+    },
+    {
+        type: 'input',
+        message: 'please describe your project',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'what dependcies do you need to run this app',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'what license is being used for this project',
+        name: 'license',
+        choices: ['MIT', 'GPLv3', 'ISC'],
+    },
+    {
+        type: 'input',
+        message: 'what is your github username',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'what is your email address',
+        name: 'email',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeToFile('/.demo/'+fileName, data, function(err) {
+    fs.writeFile( fileName, data, err=> {
         if (err) {
             return console.log(err);
         }
-        console.log ('succesfully wrote: ' + fileName);
+        console.log('successfully wrote' + fileName);
     })
-    
-}
+   }
 
 // TODO: Create a function to initialize app
 function init() {
---
+++ utils/generateMd.js
@@ -0,0 +1,68 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(data) {
+    let icon ="";
+    if (data.License == "MIT") {
+      icon = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+    } else if (data.License == 'isc') {
+      icon = '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]'
+    } else if (data.License == 'GPLv3') {
+      icon = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+    } else {
+      icon = "None"
+    }
+    return icon 
+  }
+  
+  // TODO: Create a function that returns the License link
+  // If there is no License, return an empty string
+  function renderLicenseLink(data) {
+    let link ="";
+      if (data.License == "MIT") {
+        link = '[MIT License](https://opensource.org/licenses/MIT)'
+      } else if (data.License == 'ISC') {
+        link = '[ISC License](https://opensource.org/licenses/ISC)'
+      } else if (data.License == 'GPLv3') {
+        link = '[GNU GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.en.html)'
+      } else {
+        link = "None"
+      }
+      return link
+  }
+  
+  // TODO: Create a function that returns the License section of README
+  // If there is no License, return an empty string
+  function renderLicenseSection(License) {}
+  
+  // TODO: Create a function to generate markdown for README
+  function generateMd(data) {
+    return `# ${data.Title} ${renderLicenseBadge(data)}
+   
+    # Description
+    ${data.Description}
+    # Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#License)
+    * [Contributors](#contributors)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    # Installation
+    The following dependencies are needed to be installed to run the application: 
+    ${data.Installation}
+    # Usage
+    ${data.Usage}
+    # License
+    This application runs under the ${data.License} license.
+    ${renderLicenseLink(data)}
+    # Contributors:
+    ${data.Contributors}
+    # Tests
+    How to run tests: ${data.Tests}
+    # Questions
+    If there are any questions, issues contact ${data.Username} or email at ${data.Email}
+  `;
+  }
+  
+  module.exports = generateMd;
