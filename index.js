7ccd2f133d7a01c7992209dcd0f51e492b972f43
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-08-05 23:56:29 -0500

    cloned starter and added dependencies

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// import requirements
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMd = require('./generateMd');
+const fs = require('fs');
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
